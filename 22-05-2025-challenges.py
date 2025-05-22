2b8e56940442765e29c1faac1c868cfe74f75e26
YB_BY <141259753+Yb-bY-2025@users.noreply.github.com>
2025-05-22 07:07:10 +0200

    Create 22-05-2025-challenges.py

---
+++ 22-05-2025-challenges.py
@@ -0,0 +1,14 @@
+def convert_to_seconds(hours):
+    return hours*3600
+
+def sum_of_three(a, b, c):
+    return a+b+c
+
+def is_greater_than_five(n):
+    return n>5
+
+def meters_to_centimeters(meters):
+    return meters*100
+
+def is_multiple_of_ten(n):
+    return n%10 == 0
