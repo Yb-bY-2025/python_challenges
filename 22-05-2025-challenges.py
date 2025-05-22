4d04355bc00882f0ee9bccfb545c3b856845f791
YB_BY <141259753+Yb-bY-2025@users.noreply.github.com>
2025-05-22 07:34:25 +0200

    Update 22-05-2025-challenges.py

--- 22-05-2025-challenges.py
+++ 22-05-2025-challenges.py
@@ -12,3 +12,19 @@ def meters_to_centimeters(meters):
 
 def is_multiple_of_ten(n):
     return n%10 == 0
+
+
+def convert_to_titlecase(s):
+    result = ""
+    capitalize_next = True  # Capitalize the first letter and letters after spaces
+
+    for letter in s:
+        if capitalize_next and letter.isalpha():
+            result += letter.upper()
+            capitalize_next = False
+        else:
+            result += letter
+        if letter == " ":
+            capitalize_next = True
+
+    return result
